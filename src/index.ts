9df3b96894c95f50f3502fa8a701673c5fb827ad
Renal Apriansyah <renal.apr@gmail.com>
2024-01-30 21:45:22 +0700

    standardize tabs 2

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,43 @@ import express, { Express } from "express";
 import { init } from "./init";
 import { ErrorMiddleware } from "./middlewares/error-handler-middleware";
 import { ApiKeyValidationMiddleware } from "./middlewares/api-key-validation-middleware";
-import fs from 'fs';
+import fs from "fs";
 import * as OpenApiValidator from "express-openapi-validator";
-import yaml from 'yaml';
+import yaml from "yaml";
 
 (async () => {
-    const app: Express = express();
-    const PORT = process.env.PORT || 3000;
-    const openApiPath = `${__dirname}/../docs/openapi.yaml`;
-    const file = fs.readFileSync(openApiPath, 'utf8');
-    const swaggerDocument = yaml.parse(file);
+  const app: Express = express();
+  const PORT = process.env.PORT || 3000;
+  const openApiPath = `${__dirname}/../docs/openapi.yaml`;
+  const file = fs.readFileSync(openApiPath, "utf8");
+  const swaggerDocument = yaml.parse(file);
 
-    const setupRoutes = async (app: Express): Promise<void> => {
-        const { userController } = await init();
-        
-        app.get("/", (req, res) => {
-            res.send("Hello World");
-        });
-        
-        app.use("/users/v1", ApiKeyValidationMiddleware.handle, userController.getRouter());
-    };
+  const setupRoutes = async (app: Express): Promise<void> => {
+    const { userController } = await init();
 
-    app.use(express.json());
-    app.use(OpenApiValidator.middleware({
-        apiSpec: swaggerDocument,
-        validateRequests: true,
-        validateResponses: true,
-    }));
-    await setupRoutes(app);
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
 
-    // error handler
-    app.use(ErrorMiddleware.handle);
+    app.use(
+      "/users/v1",
+      ApiKeyValidationMiddleware.handle,
+      userController.getRouter()
+    );
+  };
 
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})()
+  app.use(express.json());
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: swaggerDocument,
+      validateRequests: true,
+      validateResponses: true,
+    })
+  );
+  await setupRoutes(app);
 
+  // error handler
+  app.use(ErrorMiddleware.handle);
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})();
