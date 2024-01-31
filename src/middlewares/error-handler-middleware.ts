d98c89658b3ea8e7a9844b4d8a343741c4eb3eb1
Renal Apriansyah <renal.apr@gmail.com>
2024-01-31 09:16:27 +0700

    handle err status from openapi validator

--- src/middlewares/error-handler-middleware.ts
+++ src/middlewares/error-handler-middleware.ts
@@ -1,18 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { StandardError } from "../utils/standard-error";
+import { HttpError } from "express-openapi-validator/dist/framework/types";
 
 export class ErrorMiddleware {
   constructor() {}
   
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public static handle = (err: Error | StandardError, request: Request, res: Response, next: NextFunction): Response => {
+  public static handle = (err: Error | StandardError | HttpError, request: Request, res: Response, next: NextFunction): Response => {
     if (err instanceof StandardError) {
       return res.status(err.error_code).json({
         message: err.message,
       });
     }
-
-    return res.status(500).json({
+    
+    return res.status((err as HttpError).status || 500).json({
       message: err.message,
     });
   }
